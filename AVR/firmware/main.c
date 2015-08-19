517c8ebfad714d3cbff305d49a5b8e781310a9cd
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-08-19 10:10:51 -0500

    feat: test serial communication with computer and a push button

--- AVR/firmware/main.c
+++ AVR/firmware/main.c
@@ -1,16 +1,35 @@
 /* Name: main.c
- * Author: <insert your name here>
+ * Author: everardo sánchez
  * Copyright: <insert your copyright message here>
  * License: <insert your license reference here>
  */
 
 #include <avr/io.h>
+#include <util/delay.h>
+
+
+void usartinit();
+void enablePushButtonInput();
 
 int main(void)
 {
-    /* insert your hardware initialization here */
-    for(;;){
-        /* insert your main loop code here */
+    usartinit();
+    enablePushButtonInput();
+    while(1) {
+        if( PINB & 0x10 ) {
+          _delay_ms(200);
+          UDR0 = '0';
+        }
     }
-    return 0;   /* never reached */
+}
+
+void usartinit() {
+  UBRR0 = 6;
+  UCSR0B = (1 << TXEN0); //Enables transmiter
+  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+}
+
+void enablePushButtonInput() {
+  DDRB = 0x00;
+  PORTB = 0x00;
 }
